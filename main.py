46f6f01865443ce2fad06cdf02cc4bad96a7bdc7
MRUNAL <89269031+Mrunal1911@users.noreply.github.com>
2021-08-24 22:08:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import os
-os.system("git clone -b new https://github.com/Mrunal911/xsessionprivet.git && cd Mrunal1911 && python3 -m Bot.py")
+os.system("git clone -b new https://github.com/Mrunal911/sessionx.git && cd Mrunal1911 && python3 -m Bot.py")
  
